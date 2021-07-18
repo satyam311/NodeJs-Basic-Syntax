74826ca04f7173b1f430313e138d8c1b216e8e68
satyam <satyam987321@gmail.com>
2021-07-18 23:57:05 +0530

    Commit

---
+++ usingnpm.js
@@ -0,0 +1,8 @@
+let chalk = require("chalk");
+
+let figlet = require("figlet")
+// console.log(chalk.blue("Hello World!"));
+// console.log(chalk.red(figlet.textSync("Hello Domsto !")));
+// console.log(chalk.blue(figlet.textSync("Maine bhi kuch nhi kiya Domst")));
+
+console.log(chalk.red(figlet.textSync("Maje le Zindagi ke ")));
