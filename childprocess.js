9ce413b8d37589e6c17b07a7a79f01327332fd8b
satyam <satyam987321@gmail.com>
2021-07-18 23:55:27 +0530

    Intial Commit

---
+++ childprocess.js
@@ -0,0 +1,23 @@
+// let cp =  require("child_process");
+// const { Console } = require("console");
+// console.log("Trying to open calculator ");
+// cp.execSync("calc");
+// console.log("Calculator has opened") ;
+// console.log("Trying to open Vs code ") 
+
+// cp.execSync("code");
+// console.log("Vs Code has opend ");
+
+
+
+// cp.execSync("start chrome https://www.pepcoding.com/");
+// console.log("pepcode has opened");
+
+// cp.execSync("start brave https://twitter.com/?lang=en");
+// console.log("pepcode has opened");
+
+
+
+
+// let output = cp.execSync("node abc.js");
+// console.log("output " + output);
